9c5f910a9a2b2d45efbb3e9c4875be78a134f3d9
contrust <2001raf@gmail.com>
2021-08-02 22:54:29 +0500

    setup.py added

---
+++ cvs/__main__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import argparse
+
+from cvs.add import add
+from cvs.init import init
+
+
+def parse_arguments():
+    """
+    Parse console arguments.
+    """
+    parser = argparse.ArgumentParser(
+        prog=None if not globals().get('__spec__')
+        else f'python3 -m {__spec__.name.partition(".")[0]}'
+    )
+    parser.add_argument('-init',
+                        action='store_true',
+                        help='make .cvs folder for further work with cvs')
+    parser.add_argument('-add',
+                        metavar='path',
+                        help='updates file or folder of cvs index')
+    return parser.parse_args()
+
+
+def main():
+    """
+    Process parsed console arguments and run server.
+    """
+    try:
+        args_dict = vars(parse_arguments())
+        if args_dict['init']:
+            init()
+        elif args_dict['add']:
+            add(args_dict['add'])
+    except Exception as e:
+        print(f"{type(e).__name__} at line"
+              f" {e.__traceback__.tb_lineno} of {__file__}: {e}")
+
+
+if __name__ == '__main__':
+    main()
