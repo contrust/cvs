e17367b3c3f59309d6a5657e139d7cb68fc01448
contrust <2001raf@gmail.com>
2021-08-11 12:56:56 +0500

    more readable unload_tree.py

--- cvs/unload_tree.py
+++ cvs/unload_tree.py
@@ -1,17 +1,14 @@
 import os
 from pathlib import Path
 
-from cvs.hash_object import Tree
+from cvs.hash_object import Tree, Blob
 
 
-def unload_tree(tree: Tree, tree_path: str):
-    path = Path(tree_path)
+def unload_tree(tree: Tree, path: Path):
     if not path.exists():
         path.mkdir()
     for child_name, child in tree.children.items():
-        if child.__class__.__name__ == "Blob":
-            with open(os.path.join(tree_path, child_name),
-                      mode="wb") as blob_file:
-                blob_file.write(bytes(child))
-        elif child.__class__.__name__ == "Tree":
-            unload_tree(child, os.path.join(tree_path, child_name))
+        if isinstance(child, Blob):
+            (path / child_name).write_bytes(bytes(child))
+        elif isinstance(child, Tree):
+            unload_tree(child, (path / child_name))
