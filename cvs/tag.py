01c1798a8c538e37e813335ccd51499ae06f5570
contrust <2001raf@gmail.com>
2021-08-11 12:55:59 +0500

    more exceptions handled in tag.py

--- cvs/tag.py
+++ cvs/tag.py
@@ -1,8 +1,9 @@
 import os
 import re
 
-from cvs.branch import get_branch_content
-from cvs.config import refs_path, tags_refs_path, tags_path, head_path
+from cvs.commit import is_commit_exist
+from cvs.config import refs_path, tags_refs_path, tags_path, head_path, \
+    heads_refs_path
 from cvs.hash_object import Tag
 
 TAG_REGEX = re.compile(r'^Commit: (?P<commit_hash>\w{40})\n'
@@ -19,15 +20,21 @@ def tag(name: str, message: str) -> None:
     except FileNotFoundError:
         print('Head file does not exist.')
         return
+    if not is_commit_exist(head_content):
+        try:
+            branch_content = (heads_refs_path / head_content).read_text()
+        except FileNotFoundError:
+            print(f'Branch {head_content} does not exist.')
+            return
+        if not is_commit_exist(branch_content):
+            print(f'Branch {head_content} does not '
+                  f'attached to any existing commit.')
+            return
     try:
-        branch_content = get_branch_content('main')
+        tag_hash = Tag(message).update_hash()
     except FileNotFoundError:
-        print('Branch folder does not exist.')
+        print('Cannot create tag because some files in cvs were modified.')
         return
-    if head_content == 'main' and not branch_content:
-        print('There is no commit to attach.')
-        return
-    tag_hash = Tag(message).update_hash()
     try:
         (refs_path / "tags" / name).write_text(tag_hash)
     except FileNotFoundError:
@@ -37,7 +44,11 @@ def tag(name: str, message: str) -> None:
 
 
 def tag_list():
-    tags_names = os.listdir(str(tags_refs_path))
+    try:
+        tags_names = os.listdir(str(tags_refs_path))
+    except FileNotFoundError:
+        print('Tags folder does not exist.')
+        return
     if tags_names:
         for tag_name in tags_names:
             print(tag_name)
